f3e300aa004a025b32b0e072d45e11e581ace23f
Ilya <ilyakudrov@yandex.ru>
2021-10-02 17:26:20 +0300

    some changes in observable analysis

--- code/python/get_result_flux_schwinger.py
+++ code/python/get_result_flux_schwinger.py
@@ -5,15 +5,16 @@ import math
 import sys
 import os.path
 from numba import njit
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", ".."))
 import statistics_python.src.statistics_observables as stat
 
 conf_type = "qc2dstag"
 
 
 def get_field(data, df1):
-    x = data[['wilson-plaket-correlator',
-              'wilson-loop', 'plaket']].to_numpy()
+    x = data[['wilson-schwinger-plaket-correlator',
+              'wilson-plaket-correlator', 'wilson-loop']].to_numpy()
 
     field, err = jackknife_var(x, field_electric)
 
@@ -23,12 +24,13 @@ def get_field(data, df1):
 
     return df1
 
+
 @njit
 def electric_field_numba(x):
     n = x.shape[1]
     y = np.zeros(n)
     for i in range(n):
-        y[i] = x[0][i]/x[1][i]-x[2][i]
+        y[i] = x[0][i] / x[1][i] - x[2][i]
     return y
 
 
@@ -40,56 +42,74 @@ def field_electric(x):
 def jackknife(x, func):
     n = len(x)
     idx = np.arange(n)
-    return sum(func(x[idx != i]) for i in range(n))/float(n)
+    return sum(func(x[idx != i]) for i in range(n)) / float(n)
 
 
 def jackknife_var(x, func):
     n = len(x)
     idx = np.arange(n)
     j_est = jackknife(x, func)
-    return j_est, (n-1)/(n + 0.0) * sum((func(x[idx != i]) - j_est)**2.0
-                                        for i in range(n))
+    return j_est, (n - 1) / (n + 0.0) * sum((func(x[idx != i]) - j_est)**2.0
+                                            for i in range(n))
 
 
-T1 = [8, 10, 12]
-R1 = [8, 10, 12, 14, 16, 18]
+# T1 = [8, 10, 12]
+# R1 = [8, 10, 12, 14, 16, 18]
+T1 = [8]
+R1 = [8]
 
-mu1 = ['0.05']
-conf_size = "40^4"
-chains = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"}
-monopole="/"
+conf_type = "qc2dstag"
+# conf_type = "SU2_dinam"
+# conf_sizes = ["40^4", "32^4"]
+conf_sizes = ["32^4"]
+# conf_sizes = ["40^4"]
 
-for mu in mu1:
-    data = []
-    for chain in chains:
-        for T in T1:
-            for R in R1:
-                for i in range(1, 700):
-                    # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/s{chain}/T={T}/R={R}/electric_{i:04}"
-                    # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/T={T}/R={R}/electric_{i:04}"
-                    file_path = f"../../data/flux_tube_wilson/{monopole}/qc2dstag/{conf_size}/mu{mu}/{chain}/T={T}/R={R}/electric_{i:04}"
-                    if(os.path.isfile(file_path)):
-                        data.append(pd.read_csv(file_path))
-                        data[-1]["R"] = R
-                        data[-1]["T"] = T
-                        # data[-1]['d'] = data[-1]['d'].transform(
-                        #     lambda x: x - R/2)
+for monopole in ['/', 'monopoless', 'monopole']:
+    # for monopole in ['/']:
+    # for monopole in ['monopole']:
+    for conf_size in conf_sizes:
+        if conf_size == '40^4':
+            conf_max = 700
+            # mu1 = ['0.05', '0.35', '0.45']
+            mu1 = ['0.05']
+            chains = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"}
+        elif conf_size == '32^4':
+            conf_max = 2800
+            mu1 = ['0.00']
+            chains = {"/"}
+        for mu in mu1:
+            data = []
+            for chain in chains:
+                for T in T1:
+                    for R in R1:
+                        for i in range(0, conf_max):
+                            # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/s{chain}/T={T}/R={R}/electric_{i:04}"
+                            # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/T={T}/R={R}/electric_{i:04}"
+                            file_path = f"../../data/flux_tube_schwinger/{monopole}/qc2dstag/{conf_size}/mu{mu}/{chain}/T={T}/R={R}/electric_{i:04}"
+                            if(os.path.isfile(file_path)):
+                                data.append(pd.read_csv(file_path))
+                                data[-1]["R"] = R
+                                data[-1]["T"] = T
+                                # data[-1]['d'] = data[-1]['d'].transform(
+                                #     lambda x: x - R/2)
 
-    df = pd.concat(data)
+            df = pd.concat(data)
 
-    df1 = pd.DataFrame(columns=['field', 'err'])
+            df1 = pd.DataFrame(columns=['field', 'err'])
 
-    df1 = df.groupby(['d', 'R', 'T']).apply(get_field, df1).reset_index()
+            df1 = df.groupby(['d', 'R', 'T']).apply(
+                get_field, df1).reset_index()
 
-    df1 = df1[['d', 'R', 'T', 'field', 'err']]
+            df1 = df1[['d', 'R', 'T', 'field', 'err']]
 
-    # print(df1)
+            # print(df1)
 
-    path_output = f"../../result/flux_tube_wilson/{monopole}/qc2dstag/{conf_size}"
+            path_output = f"../../result/flux_tube_schwinger/{monopole}/qc2dstag/{conf_size}"
 
-    try:
-        os.makedirs(path_output)
-    except:
-        pass
+            try:
+                os.makedirs(path_output)
+            except:
+                pass
 
-    df1.to_csv(f"{path_output}/flux_tube_electric_mu={mu}.csv", index=False)
+            df1.to_csv(
+                f"{path_output}/flux_tube_electric_mu={mu}.csv", index=False)
--- code/python/get_result_potential_polyakov.py
+++ code/python/get_result_potential_polyakov.py
@@ -20,8 +20,8 @@ def get_field(data, df1):
 
     potential, err = stat.jackknife_var_numba(x, potential_polyakov_numba)
 
-    print("r:", data["r/a"].iloc[0], "potential:",
-          potential, "err:", math.sqrt(err))
+    # print("r:", data["r/a"].iloc[0], "potential:",
+    #       potential, "err:", math.sqrt(err))
 
     new_row = {'aV(r)': potential, 'err': math.sqrt(err)}
 
@@ -35,41 +35,60 @@ def potential_polyakov_numba(x):
     n = x.shape[1]
     y = np.zeros(n)
     for i in range(n):
-        y[i] = x[0][i]
+        if x[0][i] > 0:
+            y[i] = -math.log(x[0][i])
+        else:
+            y[i] = 0
     return y
 
 
-mu1 = ['0.05']
-conf_size = "40^4"
-chains = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"}
-monopole = "monopole"
+conf_type = "qc2dstag"
+# conf_type = "SU2_dinam"
+conf_sizes = ["40^4", "32^4"]
+# conf_sizes = ["32^4"]
+# conf_sizes = ["40^4"]
 
-for mu in mu1:
-    data = []
-    for chain in chains:
-        for i in range(1, 700):
-            # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/s{chain}/T={T}/R={R}/electric_{i:04}"
-            # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/T={T}/R={R}/electric_{i:04}"
-            file_path = f"../../data/polyakov_loop/{monopole}/qc2dstag/{conf_size}/mu{mu}/{chain}/polyakov_loop_{i:04}"
-            if(os.path.isfile(file_path)):
-                data.append(pd.read_csv(file_path, header=0,
-                            names=["r/a", "polyakov_loop"]))
+# for monopole in ['/', 'monopoless', 'monopole']:
+for monopole in ['/', 'monopoless']:
+    for conf_size in conf_sizes:
+        if conf_size == '40^4':
+            conf_max = 700
+            mu1 = ['0.05', '0.35', '0.45']
+            # mu1 = ['0.05']
+            chains = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"}
+        elif conf_size == '32^4':
+            conf_max = 2800
+            mu1 = ['0.00']
+            chains = {"/"}
+        for mu in mu1:
+            data = []
+            for chain in chains:
+                for i in range(0, conf_max):
+                    # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/s{chain}/T={T}/R={R}/electric_{i:04}"
+                    # file_path = f"../../data/flux_tube/qc2dstag/{conf_size}/HYP_APE/mu{mu}/T={T}/R={R}/electric_{i:04}"
+                    file_path = f"../../data/polyakov_loop/{monopole}/qc2dstag/{conf_size}/mu{mu}/HYP6_APE0/{chain}/polyakov_loop_{i:04}"
 
-    df = pd.concat(data)
+                    # print(file_path)
+                    if(os.path.isfile(file_path)):
+                        data.append(pd.read_csv(file_path, header=0,
+                                    names=["r/a", "polyakov_loop"]))
 
-    df1 = pd.DataFrame(columns=['aV(r)', 'err'])
+            df = pd.concat(data)
 
-    df1 = df.groupby(['r/a']).apply(get_field, df1).reset_index()
+            df1 = pd.DataFrame(columns=['aV(r)', 'err'])
 
-    df1 = df1[['r/a', 'aV(r)', 'err']]
+            df1 = df.groupby(['r/a']).apply(get_field, df1).reset_index()
 
-    # print(df1)
+            df1 = df1[['r/a', 'aV(r)', 'err']]
 
-    path_output = f"../../result/potential_polyakov/{monopole}/qc2dstag/{conf_size}"
+            # print(df1)
 
-    try:
-        os.makedirs(path_output)
-    except:
-        pass
+            path_output = f"../../result/potential_polyakov/{monopole}/qc2dstag/{conf_size}"
 
-    df1.to_csv(f"{path_output}/potential_polyakov_mu={mu}.csv", index=False)
+            try:
+                os.makedirs(path_output)
+            except:
+                pass
+
+            df1.to_csv(
+                f"{path_output}/potential_polyakov_HYP6_APE0_mu={mu}.csv", index=False)
