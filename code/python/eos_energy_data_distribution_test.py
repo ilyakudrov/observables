620ef149597350ce39d41f9ed9e3cd6a3f2556c3
Ilya <ilyakudrov@yandex.ru>
2024-05-24 15:17:23 +0300

    improved eos_energy_data_distribution_test.py

--- code/python/eos_energy_data_distribution_test.py
+++ code/python/eos_energy_data_distribution_test.py
@@ -16,77 +16,91 @@ def read_blocks(path):
                                                  'a', 'b', 'c', 'd', 'e', 'f', 'g',
                                                  'h', 'i', 'j', 'k', 'l', 'm', 'S', 'o', 'p'])
 
-def get_file_names(path, name_start):
+def get_file_names(path, args):
     files = []
-    for (dirpath, dirnames, filenames) in os.walk(base_path):
+    for (dirpath, dirnames, filenames) in os.walk(path):
         files.extend(filenames)
         break
-    return list((f for f in files if f.startswith(name_start)))
+    return list((f for f in files if f.startswith(f'block{args.block_size}-SEBxy_')))
 
 def get_dir_names(path):
     directories = []
-    for (dirpath, dirnames, filenames) in os.walk(base_path):
-        directories.extend(filenames)
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        directories.extend(dirnames)
         break
     return directories
+
+def get_data(base_path, args, df_therm):
+    df = []
+    beta_dirs = get_dir_names(f'{base_path}/{args.lattice_size}/{args.boundary}/{args.velocity}')
+    print('beta_dirs: ', beta_dirs)
+    for beta in beta_dirs:
+        if float(beta) in df_therm['beta'].unique():
+            therm_length = df_therm.loc[df_therm['beta'] == float(beta), 'therm_length'].values[0]
+        else:
+            therm_length = df_therm.loc[df_therm['beta'] == 0.00, 'therm_length'].values[0]
+        print('therm_length', therm_length)
+        chain_dirs = get_dir_names(f'{base_path}/{args.lattice_size}/{args.boundary}/{args.velocity}/{beta}')
+        print('chain_dirs: ', chain_dirs)
+        i = 0
+        df1 = []
+        for chain in chain_dirs:
+            filenames = get_file_names(f'{base_path}/{args.lattice_size}/{args.boundary}/{args.velocity}/{beta}/{chain}', args)
+            for f in filenames:
+                df1.append(read_blocks(f'{base_path}/{args.lattice_size}/{args.boundary}/{args.velocity}/{beta}/{chain}/{f}'))
+                df1[-1] = df1[-1][['S']]
+                df1[-1]['conf'] = i + 1
+                i += 1
+        df.append(pd.concat(df1))
+        df[-1]['beta'] = beta
+        # df[-1] = df[-1][df[-1]['conf'] > therm_length/args.block_size]
+    return pd.concat(df)
+
+def cut_artifacts(df, n):
+    aver = df['S'].agg('mean')
+    std = df['S'].agg('std') / np.sqrt(len(df.index))
+    df = df.loc[df['S'] >= aver - n * std]
+    return df
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--velocity')
 parser.add_argument('--lattice_size')
 parser.add_argument('--boundary')
-parser.add_argument('--name_start')
+parser.add_argument('--block_size', type=int)
 args = parser.parse_args()
 print('args: ', args)
 
-df_therm = pd.read_csv(f'/home/clusters/rrcmpi/kudrov/eos_high_precision/result/logs/{args.lattice_size}/{args.boundary}/{args.velocity}/spec_therm.log', 
-                       header=None, delimiter=' ', names=['beta', 'therm_length'])
-print('listdir: ', os.listdir
-if float(args.beta) in df_therm['beta'].unique():
-    therm_length = df_therm.loc[df_therm['beta'] == float(args.beta), 'therm_length'].values[0]
-    print('therm_length', therm_length)
-else:
-    therm_length = df_therm.loc[df_therm['beta'] == 0.00, 'therm_length'].values[0]
+# base_path = '/home/clusters/rrcmpi/kudrov/eos_high_precision/result/logs'
+base_path = '../../data/eos_high_precision'
 
-base_path = f'/home/clusters/rrcmpi/kudrov/eos_high_precision/result/logs/7x42x169sq/OBCb_cV/{args.velocity}/{args.beta}/run1001001'
-filenames = get_file_names(base_path, args.name_start)
-df = []
-for i in range(len(filenames)):
-    df.append(read_blocks(f'{base_path}/{filenames[i]}'))
-    df[-1] = df[-1][['S']]
-    df[-1]['conf'] = i + 1
-df = pd.concat(df)
+df_therm = pd.read_csv(f'{base_path}/{args.lattice_size}/{args.boundary}/{args.velocity}/spec_therm.log',
+                       header=None, delimiter=' ', names=['beta', 'therm_length'])
+print(df_therm)
+
+df = get_data(base_path, args, df_therm)
 print(df)
-df = df[df['conf'] > therm_length/10]
+df = df.groupby(['conf', 'beta']).agg('mean')
 print(df)
-df = df.groupby('conf').agg('mean')
-print(df)
-aver = df['S'].agg('mean')
-std = df['S'].agg('std') / np.sqrt(len(df.index))
-print('aver before a cut', aver, std)
-#df1 = df.loc[(df['S'] >= aver - 3 * std) & (df['S'] <= aver + 3 * std)]
-df1 = df.loc[df['S'] >= aver - 3 * std]
-aver1 = df1['S'].agg('mean')
-std1 = df1['S'].agg('std') / np.sqrt(len(df1.index))
-print('aver after a cut', aver1, std1)
-print(df['S'].min())
-#df = df.sort_values(by='S')
-print(df)
-image_path = f'../../images/eos_high_precision/7x42x169sq/OBCb_cV/{args.velocity}/{args.beta}'
+df1 = df.groubpy('beta').apply(cut_artifacts, 3)
+print(df1)
+aver = df.groupby('beta').agg('mean')
+aver1 = df1.groupby('beta').agg('mean')
+print('aver before a cut', aver)
+print('aver after a cut', aver1)
+print('min: ', df.groupby('beta').apply(min))
+image_path = f'../../images/eos_high_precision/{args.lattice_size}/{args.boundary}/{args.velocity}'
 try:
     os.makedirs(image_path)
 except:
     pass
 
-plot = seaborn.histplot(df.loc[:, 'S'])
-plot.set_xlim(aver - aver/1000, aver + aver/1000)
-fig = plot.get_figure()
-fig.savefig(f'{image_path}/S')
-plt.close()
-plot = seaborn.histplot(df1.loc[:, 'S'])
-#plot.set_xlim(aver1 - aver1/1000, aver + aver/1000)
-fig = plot.get_figure()
-fig.savefig(f'{image_path}/S1')
-plt.close()
-
-df = df.agg('mean')
-print(df)
+for beta in df['beta'].unique():
+    plot = seaborn.histplot(df.loc[df['beta'] == beta, 'S'])
+    plot.set_xlim(aver - aver/1000, aver + aver/1000)
+    fig = plot.get_figure()
+    fig.savefig(f'{image_path}/S_{beta}')
+    plt.close()
+    plot = seaborn.histplot(df1.loc[df1['beta'] == beta, 'S'])
+    fig = plot.get_figure()
+    fig.savefig(f'{image_path}/S1_{beta}')
+    plt.close()
