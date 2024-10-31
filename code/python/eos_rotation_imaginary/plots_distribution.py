d36ddb35c61108afe3e8f7952504fb62ea964c99
ilya <ilyakudrov@yandex.ru>
2024-10-31 22:17:58 +0300

    some fixes to plot distribution for eos_rotation_imaginary

--- code/python/eos_rotation_imaginary/plots_distribution.py
+++ code/python/eos_rotation_imaginary/plots_distribution.py
@@ -6,12 +6,15 @@ import seaborn
 import os
 import sys
 import argparse
+import multiprocessing
+import time
+from typing import List
 
 sys.path.append(os.path.join(os.path.dirname(
-    os.path.abspath(''))))
+    os.path.abspath(__file__)), "..", "..", ".."))
 import python_jupyter.common.plots as plots
 
-def get_dir_names(path):
+def get_dir_names(path: str) -> List[str]:
     directories = []
     for (dirpath, dirnames, filenames) in os.walk(path):
         directories.extend(dirnames)
@@ -21,8 +24,12 @@ def get_dir_names(path):
 def read_data(path, observable):
     df = pd.DataFrame()
     velocity_dirs = get_dir_names(path)
+    velocity_dirs.sort()
     for velocity in velocity_dirs:
+        print(velocity)
         beta_dirs = get_dir_names(f'{path}/{velocity}')
+        beta_dirs.sort()
+        print(beta_dirs)
         for beta in beta_dirs:
             df_tmp = pd.read_csv(f'{path}/{velocity}/{beta}/observables_distribution_aver.csv', delimiter=' ')
             df_tmp = df_tmp[['x', 'y', observable, f'{observable}_err']]
@@ -32,6 +39,8 @@ def read_data(path, observable):
     return df
 
 def gaussian_smearing(df, observable):
+    #data = df[observable].to_numpy()
+    #n = df['x'].max() * 2 + 1
     data = []
     for x in df['x'].unique():
         data.append(df[df['x'] == x][observable].to_numpy())
@@ -42,14 +51,23 @@ def gaussian_smearing(df, observable):
         df = pd.concat([df, pd.DataFrame({'x': [x - len(data) // 2] * len(data), 'y': list(range(-(len(data) // 2), len(data) // 2 + 1)), observable: data[x]})])
     return df
 
+def get_unique_pair(df):
+    df = df.loc[:, ['velocity', 'beta']]
+    df = df[~(df.duplicated(subset=['velocity', 'beta'], keep='first'))]
+    df['C'] = False
+    df = df.set_index(['velocity', 'beta']).unstack('beta', fill_value=True).stack()['C'].reset_index(level=['velocity', 'beta'])
+    df = df.groupby('beta').filter(lambda x: not x['C'].any())
+    return df['beta'].unique(), df['velocity'].unique()
+
 def heatmap_errorbar(df, subplot_ratio, observable, image_path, image_name):
-    betas = np.sort(df['beta'].unique())
-    velocities = np.sort(df['velocity'].unique())
+    #betas = np.sort(df['beta'].unique())
+    #velocities = np.sort(df['velocity'].unique())
+    betas, velocities = get_unique_pair(df)
     vmax = max(abs(df[observable].max()), abs(df[observable].min()))
     vmin = -vmax
     gs_kw = dict(height_ratios=list([subplot_ratio if i % 2 == 0 else 1 for i in range(len(velocities) * 2)]))
     fig, ax = plt.subplots(nrows=len(velocities) * 2, ncols=len(betas), gridspec_kw=gs_kw)
-    fig.set_size_inches(50, 18)
+    fig.set_size_inches(5 * len(betas), 6 * len(velocities))
     fig.suptitle(observable, fontsize=16)
     df['x'] = df['x'] - df['x'].max()//2
     df['y'] = df['y'] - df['y'].max()//2
@@ -58,19 +76,36 @@ def heatmap_errorbar(df, subplot_ratio, observable, image_path, image_name):
         for j in range(len(betas)):
             print(betas[j])
             print(velocities[i])
+            start = time.time()
             df_tmp = df[(df['beta'] == betas[j]) & (df['velocity'] == velocities[i])]
             # df1 = df_tmp.loc[(df_tmp['y'] == 0) | (df_tmp['y'] == df_tmp['x'])]
             df1 = df_tmp.loc[(df_tmp['y'] == 0)]
             df_tmp = df_tmp.loc[:, ['x', 'y', observable]]
             df_tmp = gaussian_smearing(df_tmp, observable)
+            print(df_tmp)
             df_tmp = df_tmp.pivot(columns='x', index='y', values=observable)
-            seaborn.heatmap(df_tmp, ax=ax[i * 2, j], vmin=vmin, vmax=vmax, square=True, annot=False, cmap='RdBu')
+            end = time.time()
+            print("time1 = %s" % (end - start))
+            start = time.time()
+            n = df_tmp['x'].max * 2 + 1
+            df_tmp = df_tmp[observable].to_numpy()
+            df_tmp = np.reshape(df_tmp, (n, n))
+            ax[i * 2, j].imshow(df_tmp)
+            #seaborn.heatmap(df_tmp, ax=ax[i * 2, j], vmin=vmin, vmax=vmax, square=True, annot=False, cmap='RdBu')
+            end = time.time()
+            print("heatmap time = %s" % (end - start))
+            start = time.time()
             ax[i * 2 + 1, j].errorbar(df1['x'], df1[observable], df1[f'{observable}_err'], ls='none', capsize=1, fmt='o', ms=1)
             ax[i * 2, j].set_title(f'beta = {betas[j]} | velocity = {velocities[i]}')
-    plots.save_image_plt(image_path, image_name, dpi=400)
+            end = time.time()
+            print("errorbar time = %s" % (end - start))
+    if 30 * len(betas) * len(velocities) * 400**2 > 2**16:
+        dpi = round(np.sqrt(2**16 / (30 * len(betas) * len(velocities)))) - 1
+    plots.save_image_plt(image_path, image_name, dpi=dpi)
     plt.show()
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--observable')
 parser.add_argument('--base_path')
 parser.add_argument('--lattice_size_0')
 parser.add_argument('--lattice_size_T')
@@ -79,10 +114,13 @@ parser.add_argument('--images_path')
 args = parser.parse_args()
 print('args: ', args)
 
-observable = 'Jv'
+observable = args.observable
 df_0 = read_data(f'{args.base_path}/{args.lattice_size_0}/{args.boundary}', observable)
 df_T = read_data(f'{args.base_path}/{args.lattice_size_T}/{args.boundary}', observable)
+df_tmp = pd.concat([df_0[['velocity', 'beta', 'x', 'y']], df_T[['velocity', 'beta', 'x', 'y']]])
+df_tmp = df_tmp[df_tmp.duplicated(subset=['velocity', 'beta', 'x', 'y'], keep='first')]
+df_0 = df_tmp.merge(df_0, how='left', on=['velocity', 'beta', 'x', 'y'])
+df_T = df_tmp.merge(df_T, how='left', on=['velocity', 'beta', 'x', 'y'])
 df_T[observable] = df_T[observable] - df_0[observable]
 df_T[f'{observable}_err'] = np.sqrt(df_T[f'{observable}_err'] ** 2 + df_0[f'{observable}_err'] ** 2)
-df_T = df_T[['x', 'y', observable, f'{observable}_err', 'beta', 'velocity']]
 heatmap_errorbar(df_T, 3, observable, f'{args.images_path}/{args.lattice_size_T}/{args.boundary}', f'distribution_common_{observable}')
